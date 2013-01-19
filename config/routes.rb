54cfcf3247bbbf60f6381181887ad9f9afce7e59
Brett Sanders <brettrsanders@gmail.com>
2013-01-18 22:39:25 -0800

    Tweaks to devise views, configs, and add username for login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Blog::Application.routes.draw do
+  # devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
+  devise_for :users, :path_names => 
+    { :sign_in => 'login', 
+      :sign_out => 'logout', 
+      :password => 'secret', 
+      :confirmation => 'verification' }
+
   resources :articles
   root :to => "articles#index"
 end
